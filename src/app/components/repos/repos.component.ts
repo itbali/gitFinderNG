36cbc801c5fef7ea679c4224d10a0beb4f9fc69b
itbali <alex.itbali@gmail.com>
2022-04-25 23:37:19 +0300

    исправил ошибку в названии папки (очепятка). Перевесил инпут на rxjs

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit,} from '@angular/core';
 import {RepoService} from '../../services/repo.service'
 import {Repo} from '../../Repo'
+import {fromEvent, Observable, Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  styleUrls: ['./repos.component.css'],
 })
 export class ReposComponent implements OnInit {
   repos: Repo[] = []
@@ -17,47 +19,53 @@ export class ReposComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
+    const node: Element = document.querySelector('#search-input')!
+
+    const input$ = fromEvent(node, 'input');
+    input$.subscribe({
+      next: (event: any) => {
 
 
-  onTextChange(event: any) {
+        if (event.target.value.trim().length > 1) {
+          // очищаем прошлые результаты
+          this.repos = []
+          this.errorText = ''
+          // генерируем проверку на долгую загрузку
+          let loadingID = setTimeout(() => {
+            this.loading = true
+            console.log(this.loading)
+          }, 1000)
 
-    if (event.target.value.trim().length > 1) {
-      // очищаем прошлые результаты
-      this.repos = []
-      this.errorText = ''
-      // генерируем проверку на долгую загрузку
-      let loadingID = setTimeout(() => {
-        this.loading = true
-        console.log(this.loading)
-      }, 1000)
-
-      this.repoService.getRepos(event.target.value)
-        .subscribe(
-          (repos) => {
-            // при удачной загрузке показываем репозитории и убираем текст ошибки, если он был вдруг
-            this.repos = []
-            this.repos = repos.items
-            if (repos.items.length === 0) {
-              this.errorText = 'No repos'
-            } else {
-              this.errorText = ''
-            }
-          },
-          (err) => {
-            // при неудачной загрузке показываем текст ошибки
-            this.loading = false
-            clearTimeout(loadingID)
-            this.errorText = err.error.message
-          },
-          () => {
-            // при любом исходе убираем долгую загрузку и зачищаем таймер
-            clearTimeout(loadingID)
-          }
-        )
-    } else {
-      this.errorText = 'минимум 2 символа'
-    }
+          this.repoService.getRepos(event.target.value)
+            .subscribe(
+              (repos) => {
+                // при удачной загрузке показываем репозитории и убираем текст ошибки, если он был вдруг
+                this.repos = []
+                this.repos = repos.items
+                if (repos.items.length === 0) {
+                  this.errorText = 'No repos'
+                } else {
+                  this.errorText = ''
+                }
+              },
+              (err) => {
+                // при неудачной загрузке показываем текст ошибки
+                this.loading = false
+                clearTimeout(loadingID)
+                this.errorText = err.error.message
+              },
+              () => {
+                // при любом исходе убираем долгую загрузку и зачищаем таймер
+                clearTimeout(loadingID)
+              }
+            )
+        } else {
+          this.errorText = 'минимум 2 символа'
+        }
+      },
+      error: (err: any) => console.log(`Oops... ${err}`),
+      complete: () => console.log(`Complete!`)
+    });
 
   }
 }
